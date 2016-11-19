4d01d496a4f5ab8253ca18f85fdf1c00a48d7822
Pablo Alcocer <palco003@fiu.edu>
2016-11-19 11:41:32 -0600

    Add files via upload

---
+++ clarifai-javascript-demo/js/keys.js
@@ -0,0 +1,13 @@
+// keys.js
+// Contains keys to run this demo
+// The keys should be kept secret and separate from the code
+// Enter your keys below to take the first in running this step 
+// VROOOOM VROOM
+
+// Returns an object with the required credentials
+function getKeys () {
+	return {
+		CLARIFAI_CLIENT_ID: "t8KMr5CfjkYWbX_7Kpa0w9G8wvhxu0JyV0wgzRXE",
+		CLARIFAI_CLIENT_SECRET: "Ob-m3Rt-ttrIfXMqwXBdu09QOjBsbm5ufbLEOFuP"
+	}
+}
