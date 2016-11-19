b50298039672aef08bb5ea09a2a4fe86a0af674b
Liam Patterson <patterson.liam@gmail.com>
2016-11-19 11:28:38 -0600

    Added unpacked extension
    currently background.js doesn’t compile

---
+++ background.js
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="https://sdk.clarifai.com/js/clarifai-latest.js"></script>
+<script>
+  var app = new Clarifai.App(
+    '{clientId}',
+    '{clientSecret}'
+  );
+</script>
